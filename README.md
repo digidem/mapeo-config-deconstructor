6b4ca47872535d8f01fb2974cf7f75a8218cdcfa
Luandro <luandro@digital-democracy.org>
2022-09-19 11:34:34 -0300

    feat: add deconstruction of icons.svg file and cleanup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mapeo config deconstructor
+
+## TODO:
+- use tar to extract `.mapeosettings` file
+- turn to a executable and get settings file from ARG
--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const fs = require('fs')
 const path = require('path')
+const { parseStringPromise, Builder } = require('xml2js')
 
-async function run () {
+async function desconstructPresets() {
     const file = await fs.readFileSync('./presets.json')
     const json = await JSON.parse(file)
     Object.keys(json).map(async i => {
-        console.log(i)
         if (i === 'presets' || i === 'fields') {
             Object.entries(json[i]).map(async ([key, value]) => {
-                // console.log(key, value)
-                await fs.mkdirSync(path.join(__dirname, i), {recursive: true})
+                await fs.mkdirSync(path.join(__dirname, i), { recursive: true })
                 const filePath = path.join(__dirname, i, `${key}.json`)
-                console.log(filePath)
                 await fs.writeFileSync(filePath, JSON.stringify(value))
             })
         } else if (i === 'defaults') {
@@ -19,9 +17,28 @@ async function run () {
             await fs.writeFileSync(filePath, JSON.stringify(json[i]))
         }
     })
-    // console.log('FILE', json.presets)
-    // console.log('FILE', json.defaults)
-
 }
 
-run()
+async function desconstructSvgSprite() {
+    const file = await fs.readFileSync('./icons.svg').toString()
+    const parsed = await parseStringPromise(file)
+    await fs.mkdirSync(path.join(__dirname, 'icons'), { recursive: true })
+    if (parsed?.svg?.symbol.length > 0) {
+        parsed.svg.symbol.forEach(async e => {
+            const builder = new Builder()
+            let newSvg = {
+                svg: {
+                    ...e
+                }
+            }
+            const newXml = builder.buildObject(newSvg);
+            const splitName = e['$'].id.split('-12')
+            const fileName = splitName.length > 1 ? `${splitName[0]}-24px.svg` : `${splitName[0]}-100px.svg`
+            const filePath = path.join(__dirname, 'icons', fileName)
+            await fs.writeFileSync(filePath, newXml)
+        });
+    }
+}
+
+desconstructPresets()
+desconstructSvgSprite()
