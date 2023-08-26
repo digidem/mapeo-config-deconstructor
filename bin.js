1efb9a3f7aff6893398c351fae962a2787cf6a71
Luandro <luandro@digital-democracy.org>
2023-08-26 08:26:29 -0300

    chore(bin.js): reformat code using Prettier feat(bin.js): add support for creating output folder if it doesn't exist feat(bin.js): add console logs to indicate progress and completion of build process fix(index.js): change require statements to use double quotes for consistency refactor(index.js): rename desconstructPresets function to deconstructPresets for clarity refactor(index.js): rename desconstructSvgSprite function to deconstructSvgSprite for clarity refactor(index.js): rename copyFiles function to copyFilesToOutput for clarity chore(package.json): add lint script to run Prettier on all JavaScript files chore(package.json): add prettier as a dev dependency

--- bin.js
+++ bin.js
@@ -1,22 +1,33 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
-const { desconstructPresets, desconstructSvgSprite } = require('./index.js')
-const configFolder = process.argv[2]
-let outputFolder = process.argv[3]
+const fs = require("fs");
+const path = require("path");
+const { desconstructPresets, desconstructSvgSprite } = require("./index.js");
+const { copyFiles } = require("./index.js");
 
-if (!configFolder) {
-    console.error('Please provide a config folder as the first argument.')
-    process.exit(1)
+const configFolder = process.argv[2];
+let outputFolder = process.argv[3];
+
+async function run() {
+  if (!configFolder) {
+    console.error("Please provide a config folder as the first argument.");
+    process.exit(1);
+  }
+
+  if (!outputFolder) {
+    const { name } = JSON.parse(
+      fs.readFileSync(path.join(configFolder, "metadata.json")),
+    );
+    outputFolder = path.join(configFolder, name);
+    if (!fs.existsSync(outputFolder)) {
+      fs.mkdirSync(outputFolder, { recursive: true });
+    }
+  }
+
+  console.log("Building project", outputFolder);
+  await desconstructPresets(configFolder, outputFolder);
+  await desconstructSvgSprite(configFolder, outputFolder);
+  await copyFiles(configFolder, outputFolder);
+  console.log("Done!");
 }
 
-if (!outputFolder) {
-    const packageJson = JSON.parse(fs.readFileSync(path.join(configFolder, 'package.json')))
-    outputFolder = path.join(configFolder, packageJson.name)
-}
-
-const { copyFiles } = require('./index.js')
-
-desconstructPresets(configFolder, outputFolder)
-desconstructSvgSprite(configFolder, outputFolder)
-copyFiles(configFolder, outputFolder)
+run();
--- index.js
+++ index.js
@@ -1,58 +1,63 @@
-const fs = require('fs')
-const path = require('path')
-const { parseStringPromise, Builder } = require('xml2js')
+const fs = require("fs");
+const path = require("path");
+const { parseStringPromise, Builder } = require("xml2js");
 
 async function desconstructPresets(configFolder, outputFolder) {
-    const file = await fs.readFileSync(path.join(configFolder, 'presets.json'))
-    const json = await JSON.parse(file)
-    Object.keys(json).map(async i => {
-        if (i === 'presets' || i === 'fields') {
-            Object.entries(json[i]).map(async ([key, value]) => {
-                await fs.mkdirSync(path.join(configFolder, i), { recursive: true })
-                const filePath = path.join(configFolder, i, `${key}.json`)
-                await fs.writeFileSync(filePath, JSON.stringify(value))
-            })
-        } else if (i === 'defaults') {
-            const filePath = path.join(configFolder, `${i}.json`)
-            await fs.writeFileSync(filePath, JSON.stringify(json[i]))
-        }
-    })
+  const file = await fs.readFileSync(path.join(configFolder, "presets.json"));
+  const json = await JSON.parse(file);
+  Object.keys(json).map(async (i) => {
+    if (i === "presets" || i === "fields") {
+      Object.entries(json[i]).map(async ([key, value]) => {
+        await fs.mkdirSync(path.join(configFolder, i), { recursive: true });
+        const filePath = path.join(configFolder, i, `${key}.json`);
+        await fs.writeFileSync(filePath, JSON.stringify(value));
+      });
+    } else if (i === "defaults") {
+      const filePath = path.join(configFolder, `${i}.json`);
+      await fs.writeFileSync(filePath, JSON.stringify(json[i]));
+    }
+  });
 }
 
 async function desconstructSvgSprite(configFolder, outputFolder) {
-    const file = await fs.readFileSync(path.join(configFolder, 'icons.svg')).toString()
-    const parsed = await parseStringPromise(file)
-    await fs.mkdirSync(path.join(configFolder, 'icons'), { recursive: true })
-    if (parsed?.svg?.symbol.length > 0) {
-        parsed.svg.symbol.forEach(async e => {
-            const builder = new Builder()
-            let newSvg = {
-                svg: {
-                    ...e
-                }
-            }
-            const newXml = builder.buildObject(newSvg);
-            const splitName = e['$'].id.split('-12')
-            const fileName = splitName.length > 1 ? `${splitName[0]}-24px.svg` : `${splitName[0]}-100px.svg`
-            const filePath = path.join(configFolder, 'icons', fileName)
-            await fs.writeFileSync(filePath, newXml)
-        });
-    }
+  const file = await fs
+    .readFileSync(path.join(configFolder, "icons.svg"))
+    .toString();
+  const parsed = await parseStringPromise(file);
+  await fs.mkdirSync(path.join(configFolder, "icons"), { recursive: true });
+  if (parsed?.svg?.symbol.length > 0) {
+    parsed.svg.symbol.forEach(async (e) => {
+      const builder = new Builder();
+      let newSvg = {
+        svg: {
+          ...e,
+        },
+      };
+      const newXml = builder.buildObject(newSvg);
+      const splitName = e["$"].id.split("-12");
+      const fileName =
+        splitName.length > 1
+          ? `${splitName[0]}-24px.svg`
+          : `${splitName[0]}-100px.svg`;
+      const filePath = path.join(configFolder, "icons", fileName);
+      await fs.writeFileSync(filePath, newXml);
+    });
+  }
 }
 
 const copyFiles = async (configFolder, outputFolder) => {
-    const filesToCopy = ['translation.json', 'style.css', 'metadata.json']
-    for (const file of filesToCopy) {
-        const src = path.join(configFolder, file)
-        const dest = path.join(outputFolder, file)
-        if (fs.existsSync(src)) {
-            fs.copyFileSync(src, dest)
-        }
+  const filesToCopy = ["translation.json", "style.css", "metadata.json"];
+  for (const file of filesToCopy) {
+    const src = path.join(configFolder, file);
+    const dest = path.join(outputFolder, file);
+    if (fs.existsSync(src)) {
+      fs.copyFileSync(src, dest);
     }
-}
+  }
+};
 
 module.exports = {
-    desconstructPresets,
-    desconstructSvgSprite,
-    copyFiles
-}
+  desconstructPresets,
+  desconstructSvgSprite,
+  copyFiles,
+};
