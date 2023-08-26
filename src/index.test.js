8e7d9ad2d6958ca126d9a0849377a8879260d82b
Luandro <luandro@digital-democracy.org>
2023-08-26 10:47:49 -0300

    aider: Refactored `extractConfig` function to include tests for different scenarios and use `sinon` library to stub `fs` and `tar` modules.
    # Aider chat conversation:
    
    ASSISTANT: To write the test for the `extractConfig` function, we need to consider the different scenarios that the function handles. Here are the scenarios I identified:
    
    1. When `configPath` is not provided, the function should throw an error.
    2. When `configPath` is a file, the function should extract the config and return the paths.
    3. When `configPath` is a directory, the function should return the paths without extraction.
    
    We will use the `chai` assertion library to write the tests. We will also use the `sinon` library to stub the `fs` and `tar` modules, as we don't want to perform actual file system operations during testing.
    
    Here is the test for the `extractConfig` function:
    
    ```javascript
    src/index.test.js
    <<<<<<< HEAD
    describe("extractConfig", () => {
      it("should take a tar file and ouput a config folder", () => {
        // Test goes here
        // expect(extractConfig()).to...
      });
    });
    =======
    const sinon = require('sinon');
    const fs = require('fs');
    const tar = require('tar');
    
    describe("extractConfig", () => {
      let fsStub, tarStub;
    
      beforeEach(() => {
        fsStub = sinon.stub(fs, 'lstatSync');
        tarStub = sinon.stub(tar, 'x');
      });
    
      afterEach(() => {
        fsStub.restore();
        tarStub.restore();
      });
    
      it("should throw an error if configPath is not provided", async () => {
        try {
          await extractConfig();
        } catch (error) {
          expect(error.message).to.equal("Please provide a configPath as the first argument.");
        }
      });
    
      it("should extract config if configPath is a file", async () => {
        fsStub.returns({ isFile: () => true });
        tarStub.resolves();
    
        const result = await extractConfig('path/to/config.tar');
        expect(result.configFolder).to.exist;
        expect(result.outputFolder).to.exist;
      });
    
      it("should return paths without extraction if configPath is a directory", async () => {
        fsStub.returns({ isFile: () => false, isDirectory: () => true });
    
        const result = await extractConfig('path/to/config');
        expect(result.configFolder).to.exist;
        expect(result.outputFolder).to.exist;
      });
    });
    >>>>>>> updated
    ```
    
    Please note that you need to install the `sinon` library to use it in the tests. You can install it by running `npm install --save-dev sinon`.

--- src/index.test.js
+++ src/index.test.js
@@ -36,9 +36,45 @@ describe("createPackageJson", () => {
   });
 });
 
+const sinon = require('sinon');
+const fs = require('fs');
+const tar = require('tar');
+
 describe("extractConfig", () => {
-  it("should take a tar file and ouput a config folder", () => {
-    // Test goes here
-    // expect(extractConfig()).to...
+  let fsStub, tarStub;
+
+  beforeEach(() => {
+    fsStub = sinon.stub(fs, 'lstatSync');
+    tarStub = sinon.stub(tar, 'x');
+  });
+
+  afterEach(() => {
+    fsStub.restore();
+    tarStub.restore();
+  });
+
+  it("should throw an error if configPath is not provided", async () => {
+    try {
+      await extractConfig();
+    } catch (error) {
+      expect(error.message).to.equal("Please provide a configPath as the first argument.");
+    }
+  });
+
+  it("should extract config if configPath is a file", async () => {
+    fsStub.returns({ isFile: () => true });
+    tarStub.resolves();
+
+    const result = await extractConfig('path/to/config.tar');
+    expect(result.configFolder).to.exist;
+    expect(result.outputFolder).to.exist;
+  });
+
+  it("should return paths without extraction if configPath is a directory", async () => {
+    fsStub.returns({ isFile: () => false, isDirectory: () => true });
+
+    const result = await extractConfig('path/to/config');
+    expect(result.configFolder).to.exist;
+    expect(result.outputFolder).to.exist;
   });
 });
